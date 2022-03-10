2c0406376d075819978a91b3226463f2ae63e458
oem <oem@DESKTOP-P9U1V2P>
2022-03-10 13:47:34 +0100

    Dodaj pliki projektów.

---
+++ karta/Program.cs
@@ -0,0 +1,32 @@
+﻿Random r = new Random();
+int liczba = r.Next(0, 10);
+double a;
+Console.WriteLine("zgadnij liczbe");
+a = double.Parse(Console.ReadLine());
+if (a < liczba)
+{
+    Console.WriteLine("Podana liczba jest za mała");
+}
+else if (a > liczba)
+{
+    Console.WriteLine("Podana liczba jest za duża");
+
+}
+else
+    Console.WriteLine("Zgadłeś");
+while (a != liczba)
+{
+    Console.WriteLine("Zgadnij liczbe");
+    a = double.Parse(Console.ReadLine());
+    if (a < liczba)
+    {
+        Console.WriteLine("Podana liczba jest za mała");
+    }
+    else if (a > liczba)
+    {
+        Console.WriteLine("Podana liczba jest za duża");
+
+    }
+    else
+        Console.WriteLine("Zgadłeś");
+}
